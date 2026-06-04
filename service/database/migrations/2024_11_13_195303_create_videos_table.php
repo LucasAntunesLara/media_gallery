5aa4ac66926958fb6f12c66282357641fab4e492
Lucas Antunes <lucasantuneslara06@gmail.com>
2026-06-04 07:28:19 -0300

    refactor: [Video Migration] remover nullable de isFavourite

--- service/database/migrations/2024_11_13_195303_create_videos_table.php
+++ service/database/migrations/2024_11_13_195303_create_videos_table.php
@@ -12,7 +12,7 @@ return new class extends Migration {
             $table->string('url', 255);
             $table->string('thumbnail', 255)->default('');
             $table->string('tags')->nullable();
-            $table->boolean('isFavourite')->nullable()->default(false);
+            $table->boolean('isFavourite')->default(false);
             $table->boolean('isPrivate')->nullable()->default(false);
             $table->timestamps();
         });
